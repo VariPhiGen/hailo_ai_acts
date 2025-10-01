4e80f45e861b2a9186be66339a46c32daee67f03
nitishmishra006 <nitishmishra006@gmail.com>
2025-10-01 19:09:58 +0530

    New Updates Fixed

--- setup_env.sh
+++ setup_env.sh
@@ -11,7 +11,7 @@
 # After sourcing, your PYTHONPATH will be set for the current terminal session.
 
 # Define virtual environment name
-VENV_NAME="SVDS"
+VENV_NAME="aiacts"
 
 # Function to check if the script is being sourced
 is_sourced() {
